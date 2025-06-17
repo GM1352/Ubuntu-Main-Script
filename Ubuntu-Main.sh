f203476768074c353faad6a070f2be9a0dc70418
GM1352 <danekoravecz@gmail.com>
2025-06-17 12:01:54 +0200

    V0.2.4

--- Ubuntu-Main.sh
+++ Ubuntu-Main.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/bash
-echo sudo apt install -y wget apt-transport-https gpg
-echo sudo wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
-echo sudo echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list 
-echo sudo apt update 
-echo sudo apt upgrade
-echo sudo apt install net-tools btop
-echo sudo apt install temurin-8-jdk temurin-17-jdk temurin-21-jdk
+sudo apt install -y wget apt-transport-https gpg
+sudo wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
+sudo echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list 
+sudo apt update 
+sudo apt upgrade
+sudo apt install net-tools btop
+sudo apt install temurin-8-jdk temurin-17-jdk temurin-21-jdk
