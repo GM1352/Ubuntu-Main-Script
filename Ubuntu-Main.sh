b2cb719173c4dbef0a8ab394281fc0201b49b887
GM1352 <danekoravecz@gmail.com>
2025-06-17 12:14:21 +0200

    V0.2.5

--- Ubuntu-Main.sh
+++ Ubuntu-Main.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/bash
 sudo apt install -y wget apt-transport-https gpg
+echo "deb [arch=amd64] https://some.repository.url focal main" | sudo tee /etc/apt/sources.list.d/adoptium.list > /dev/null
 sudo wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
 sudo echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list 
 sudo apt update 
